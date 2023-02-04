7f27e27296257350bc88296bd5dafa5edd260956
Ryan Cooney <rcooney@users.noreply.github.com>
2023-02-04 14:40:10 -0800

    feat: add Open Window activity (#3)
    Co-authored-by: Ryan Cooney <ryan.cooney@vertigis.com>

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as CreateObjectUrl } from "./activities/CreateObjectUrl";
 export { default as DispatchCustomEvent } from "./activities/DispatchCustomEvent";
 export { default as GenerateRandomUuid } from "./activities/GenerateRandomUuid";
 export { default as HashMessage} from "./activities/HashMessage";
+export { default as OpenWindow} from "./activities/OpenWindow";
 export { default as SendBeacon} from "./activities/SendBeacon";
