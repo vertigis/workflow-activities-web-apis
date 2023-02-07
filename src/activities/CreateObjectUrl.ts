27b74c6ecf7bba1d3e83c4570124cadadd525e36
Ryan Cooney <rcooney@users.noreply.github.com>
2023-02-07 13:47:52 -0800

    feat!: add MIME type allow list to Create Object URL (#4)

--- src/activities/CreateObjectUrl.ts
+++ src/activities/CreateObjectUrl.ts
@@ -1,11 +1,44 @@
 import type { IActivityHandler } from "@geocortex/workflow/runtime";
 
+const defaultAllowedMimeTypes = new Set([
+    "application/json",
+    "application/octet-stream",
+    "application/pdf",
+    "application/rtf",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/zip",
+    "audio/mpeg",
+    "audio/wav",
+    "audio/webm",
+    "image/bmp",
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "image/tiff",
+    "image/webp",
+    "text/csv",
+    "text/plain",
+    "video/mp4",
+    "video/mpeg",
+    "video/webm",
+]);
+
 interface CreateObjectUrlInputs {
     /**
      * @description The content.
      * @required
      */
-    content: Blob;
+    content: File | Blob;
+
+    /**
+     * @displayName Allowed MIME Types
+     * @description A list of MIME types to allow. If not specified a list of common and safe MIME types is used.
+     * WARNING: including MIME types like "text/html" or "image/svg+xml" that can execute JavaScript increases the risk of XSS attacks.
+     * Don't use this property if you don't understand the risks.
+     */
+    allowedMimeTypes?: string[];
 }
 
 interface CreateObjectUrlOutputs {
@@ -31,7 +64,20 @@ interface CreateObjectUrlOutputs {
  */
 export default class CreateObjectUrlActivity implements IActivityHandler {
     execute(inputs: CreateObjectUrlInputs): CreateObjectUrlOutputs {
-        const { content } = inputs;
+        const { allowedMimeTypes, content } = inputs;
+
+        let allow = false;
+        const mimeType = content.type.toLowerCase();
+        if (allowedMimeTypes) {
+            allow = allowedMimeTypes.includes(mimeType);
+        } else {
+            allow = defaultAllowedMimeTypes.has(mimeType);
+        }
+
+        if (!allow) {
+            throw new Error(`MIME type '${content.type}' is not allowed`);
+        }
+
         const url = URL.createObjectURL(content);
         return {
             url,
